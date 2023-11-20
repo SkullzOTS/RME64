0ef1eec281bcc7c580f75e236f41f91d97db389f
SkullzOTS <86809689+SkullzOTS@users.noreply.github.com>
2023-11-19 22:39:39 -0300

    Update dat_debug_view.cpp

--- source/dat_debug_view.cpp
+++ source/dat_debug_view.cpp
@@ -73,7 +73,7 @@ void DatDebugViewListBox::OnDrawItem(wxDC& dc, const wxRect& rect, size_t n) con
 		dc.SetTextForeground(wxColor(0x00, 0x00, 0x00));
 	}
 
-	dc.DrawText(wxString() << n, rect.GetX() + 40, rect.GetY() + 6);
+	dc.DrawText(wxString() << n, rect.GetX() + 72, rect.GetY() + 6);
 }
 
 wxCoord DatDebugViewListBox::OnMeasureItem(size_t n) const
